bd92543e6db5416c99babd9fd12b7e4300f824f0
shakilhassanBD <hasanshakil564@gmail.com>
2023-10-16 17:06:46 +0600

    first commit

---
+++ module_2/module02.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main()
+{
+    // int tk;
+    // scanf("%d", &tk);
+    // if (tk >= 5000)
+    // {
+    //     printf("cox's bazar jabo\n");
+    //     if (tk >= 10000)
+    //     {
+    //         printf("saint martin jabo\n");
+    //     }
+
+    //     if (tk < 10000 && tk > 5000)
+    //     {
+    //         printf("back from cox's bazar\n");
+    //     }
+    //     else{
+    //         printf("ferot jai ga");
+    //     }
+    // }
+
+    // else
+    // {
+    //     printf("eto sokh valo na");
+    // }
+
+    // arithmatic operator............
+
+    // int a, b, sum, minus, odd;
+    // a = 45;
+    // b = 10;
+    // sum = a + b;
+    // minus = a - b;
+    // odd = a * b;
+
+    // printf("%d %d %d %f %d", sum, minus, odd);
+
+    // int a, b;
+    // a = 22;
+    // b= 4;
+    // float divd = a*1.0/b;
+    // printf("%0.2f", divd);
+
+    return 0;
+}
---
+++ module_3/looping.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    //     int i, sum, n;
+    //     sum = 0;
+    //     scanf("%d", &n);
+    //     for (i = 0; i <= n; i++)
+    //     {
+    //         sum = sum + i;
+    //     }
+    //         printf("%d\n", sum);
+
+    //...................while loop..........
+    // int i;
+    // i = 0;
+    // while (i<=5)
+    // {
+    //     printf("%d i am sorry.\n",i);
+    //     i++;
+    // }
+
+    //......................do while loop.............
+
+    // int i;
+    // i = 1;
+    // do
+    // {
+    //     printf("%d i love you.\n", i);
+    //     i++;
+    // } while (i <= 10);
+
+    // return 0;
+
+}
